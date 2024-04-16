b5a3d73a0fbf079343ecafb467c0b3a2196beae8
jmontagne2023 <montagne.julian974@gmail.com>
2024-04-16 10:27:31 +0200

    fix: crud ok hormis la modif à check

--- src/main/java/fr/eni/encheres2/controller/CategorieController.java
+++ src/main/java/fr/eni/encheres2/controller/CategorieController.java
@@ -3,17 +3,10 @@ package fr.eni.encheres2.controller;
 import fr.eni.encheres2.dto.CategorieDto;
 import fr.eni.encheres2.service.CategorieService;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 
 @RestController
@@ -32,58 +25,36 @@ public class CategorieController {
     }
 
     @GetMapping("/{noCategorie}")
-    public String afficherCategorieParNo(@PathVariable("noCategorie") @NotBlank Long noCategorie, Model model) {
-        model.addAttribute("categorie", categorieService.consulterCategorieParNo(noCategorie));
-        return "categorie";
-    }
-
-    @GetMapping("/nouvelleCategorie")
-    public String afficherFormulaireCreationCategorie(Model model) {
-        model.addAttribute("article", new CategorieDto());
-        return "nouvelCategorie";
+    public ResponseEntity<CategorieDto> afficherCategorieParNo(@PathVariable("noCategorie") @NotNull Long noCategorie) {
+        CategorieDto categorieDto = categorieService.consulterCategorieParNo(noCategorie);
+        if (categorieDto != null) {
+            return ResponseEntity.ok(categorieDto);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping("/creer")
-    public String ajouterCategorie(@Valid CategorieDto categorie, BindingResult bindingResult) {
-        categorieService.creerCategorie(categorie);
+    public ResponseEntity<CategorieDto> ajouterCategorie(@Valid CategorieDto categorie) {
+        CategorieDto categorieDto = categorieService.creerCategorie(categorie);
 
-        if (bindingResult.hasErrors()) {
-            return "creer";
+        if (categorieDto != null) {
+            return ResponseEntity.ok(categorieDto);
+        } else {
+            return ResponseEntity.notFound().build();
         }
-        return "redirect:/categories";
     }
 
-    @GetMapping("/{noCategorie}/modifier")
-    public String getFilmEditForm(@PathVariable("noCategorie") Long noCategorie, Model model) {
-        model.addAttribute("categorie", categorieService.consulterCategorieParNo(noCategorie));
-
-        return "creer";
+    @PutMapping("/{noCategorie}/modifier")
+    public ResponseEntity<CategorieDto> modifierCategorie(@PathVariable @NotNull Long noCategorie, @RequestBody CategorieDto categorieDto) {
+        categorieDto.setNoCategorie(noCategorie);
+        categorieService.modifierCategorie(categorieDto);
+        return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/{noCategorie}/supprimer")
-    public String supprimerUneCategorie(@PathVariable Long noCategorie, Model model) {
-
-        CategorieDto categorie = categorieService.consulterCategorieParNo(noCategorie);
-
-        model.addAttribute("message", "Êtes vous sur de vouloir supprimer la catégorie : " + categorie.getLibelle());
-        model.addAttribute("action", "/categories/" + noCategorie + "/supprimer");
-        model.addAttribute("back", "/categories");
-
-        // 2 - on redirige sur une page de confirmation ou l'utilisateur va devoir valider son choix
-        return "confirmation";
-    }
-
-    /**
-     * Est appelé lorsqu'on a validé dans le template "confirmation" qu'on souhaite effectuer la suppression de la catégorie
-     * Je vais recupérer un no en paramètre d'url
-     */
-    @PostMapping("/{noCategorie}/supprimer")
-    public String supprimerFilm(@PathVariable Long noCategorie) {
-
+    @DeleteMapping("/{noCategorie}/supprimer")
+    public ResponseEntity<CategorieDto> supprimerUneCategorie(@PathVariable Long noCategorie) {
         categorieService.supprimerCategorie(noCategorie);
-
-        return "redirect:/";
+        return ResponseEntity.ok().build();
     }
-
-
 }
