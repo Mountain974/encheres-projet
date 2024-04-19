63e12f90eaccff7f135fd7011ff5305eb7db03f0
Marion Pougnard <marion.pougnard@protonmail.com>
2024-04-19 08:11:14 +0200

    feat: utilisation queryKey

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import '../App.css'
-import {useQuery} from "@tanstack/react-query";
+import {useQuery, useQueryClient} from "@tanstack/react-query";
 import {Encheres} from "../fragments/Encheres.jsx";
 
 
 function Home(isConnected) {
-
+    const queryClient = useQueryClient()
     const articlesEnCours = useQuery({
         queryKey:["articles"],
         queryFn: () => fetch("./api/articles")
@@ -27,7 +27,10 @@ function Home(isConnected) {
 
     return (
         <>
-            <Encheres articles={articlesEnCours.data} retraits={retraits.data} isConnected />
+            <Encheres articles={articlesEnCours.data} retraits={retraits.data} isConnected/>
+            <button className="btn btn-primary"
+                    onClick={() => queryClient.invalidateQueries({queryKey: ["articles"]})}>Reload
+            </button>
         </>
 
     )
